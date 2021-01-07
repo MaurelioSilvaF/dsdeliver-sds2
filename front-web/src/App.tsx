46369cf8e6492bf97c3b49b4a44e5dfdd0d4b52e
Maurelio <maureliosilvaf@gmail.com>
2021-01-07 18:34:08 -0300

    Create front-web initial files

---
+++ front-web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Hello from './Hello'
+
+function App() {
+  return (
+    
+      <Hello message="Maurelio"/>
+   
+  );
+}
+
+export default App;
