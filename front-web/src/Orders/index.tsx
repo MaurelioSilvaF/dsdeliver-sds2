ceca3d5286e54af86665bfd133b0ac56c2037e21
Maurelio <maureliosilvaf@gmail.com>
2021-01-09 12:21:01 -0300

    Add Product-Card / Product-List / GetProducts

--- front-web/src/Orders/index.tsx
+++ front-web/src/Orders/index.tsx
@@ -1,13 +1,32 @@
+import { useEffect, useState } from 'react';
 import './styles.css'
-import {ReactComponent as Logo} from './logo.svg';
 import StepsHeader from './StepsHeader';
+import { Product } from './type';
+import { fetchProducts } from '../api';
+import ProductsList from './ProductsList';
+
 
 function Orders() {
 
+    const [products, setProducts] = useState<Product[]>([]);
+    console.log(products);
+
+    useEffect(
+        () => {
+            fetchProducts()
+                .then(
+                    response => setProducts(response.data)
+                )
+                .catch(
+                    error => console.log(error)
+                )
+        }, []
+    );
     return (
         
         <div className="orders-container">
             <StepsHeader />
+            <ProductsList products={products}/>
         </div>
         
     )
