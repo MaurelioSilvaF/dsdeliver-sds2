af5c7bcba6ba67c90e266c849b3a59227a1e4057
Maurelio <maureliosilvaf@gmail.com>
2021-01-13 23:13:27 -0300

    Mobile - Routes and Orders

---
+++ front-mobile/src/Orders/index.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Header from '../Header';
+
+function Orders() {
+
+    return (
+      <>
+      <Header />
+        <View style={styles.container}>
+          <Text>sdfsdkfnsdlkfnsdkf fsdlkfn sdlkfn sd</Text>
+        </View>
+        </>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+      marginTop: '5%',
+      alignItems: 'center'
+      }
+});
+
+export default Orders;
